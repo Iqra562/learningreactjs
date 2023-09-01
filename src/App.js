4448e9ad62c0b7a31803cfaf04139675f3e49b80
Iqra <iqra44938@gmail.com>
2023-09-01 16:18:28 +0500

    convert text  to uppercase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar  from './components.js/Navbar';
+import Form from './components.js/form';
 function App() {
   return (
    <>
  <Navbar title="ReactTitle"/>
+ <div className="container"> 
+   <Form heading="Enter your text"/>
+
+ </div>
    </>
   );
 }
